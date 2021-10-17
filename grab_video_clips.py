95f78cadb53ce572c03aaec33a337d850486e8c0
Ian Afflerbach <ianafflerbach42@gmail.com>
2021-10-17 13:39:06 -0500

    Fixed video trimming

--- grab_video_clips.py
+++ grab_video_clips.py
@@ -11,17 +11,25 @@ arr = [arr[:, i] for i in range(0, np.shape(arr)[1])]
 for x in arr:
     print("Downloading Video: %d" % x[0])
 
+    video = None;
     try:
         video = YouTube(x[1])
-        tag = video.streams.filter(file_extension = "mp4")[0].itag
-        video.streams.get_by_itag(tag).download(output_path="raw_videos", filename="video_" + str(x[0]) + ".mp4")
-
-        with VideoFileClip("raw_videos/video_" + str(x[0]) + ".mp4") as clip:
-            clip = clip.subclip(x[2], x[2] + 60)
-            clip.write_videofile("trimmed_videos/video_" + str(x[0]) + "_trimmed.mp4")
     except:
-        print("\rError Downloading Video: %d" % x[0])
-    
+        print("Error Downloading Video: %d" % x[0])
+        continue
+        
+    tag = video.streams.filter(file_extension = "mp4")[0].itag
+    video.streams.get_by_itag(tag).download(output_path="raw_videos", filename="video_" + str(x[0]) + ".mp4")
+
+    try:
+        with VideoFileClip("raw_videos/video_" + str(x[0]) + ".mp4") as clip:
+            start = x[2]
+            end = min(x[2] + 60, clip.end);
+            clip = clip.subclip(start, end)
+            clip.write_videofile("trimmed_videos/video_" + str(x[0]) + "_trimmed.mp4")
+    except Exception as e:
+        print("\nError Trimming Video: %d" % x[0])
+        quit()
     
 print("Finished Downloading Videos")
 
